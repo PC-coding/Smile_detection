44cd20bbb3e42075672ef3c1d477f3223d2e3475
PC-coding <partapchauhan8@gmail.com>
2021-03-15 17:52:01 -0400

    reading webcam feed -> showing image in python program

--- smile_detection.py
+++ smile_detection.py
@@ -6,7 +6,11 @@ face_classifier = cv2.CascadeClassifier('haarscascade_frontalface_default.xml')
 # webcam feed
 webcam = cv2.VideoCapture(0)
 
+# reading webcam feed
 successful_frame, frame = webcam.read()
+
+# show image in python program
+cv2.imshow('Smile Detection App', frame)
 
 
 print('successful')
