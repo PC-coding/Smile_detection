3055c808760c90b36e6beda5f4f8832bebb3212f
PC-coding <partapchauhan8@gmail.com>
2021-03-15 18:16:33 -0400

    while loop added + cleanup code

--- smile_detection.py
+++ smile_detection.py
@@ -6,13 +6,18 @@ face_classifier = cv2.CascadeClassifier('haarscascade_frontalface_default.xml')
 # webcam feed
 webcam = cv2.VideoCapture(0)
 
-# reading webcam feed
-successful_frame, frame = webcam.read()
+while True:
+    # reading webcam feed
+    successful_frame, frame = webcam.read()
 
-# show image in python program
-cv2.imshow('Smile Detection App', frame)
+    # show image in python program
+    cv2.imshow('Smile Detection App', frame)
 
-# display, wait until you press a key to close program
-cv2.waitKey
+    # display, wait until you press a key to close program
+    cv2.waitKey
+
+# cleanup -> release webcam use, close python windows
+webcam.release()
+cv2.destroyAllWindows()
 
 print('successful')
