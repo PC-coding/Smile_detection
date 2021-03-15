aae97d4dfbed812d8bdbc50c6166b32304ac0aaf
PC-coding <partapchauhan8@gmail.com>
2021-03-15 17:49:08 -0400

    grabbing webcam feed -> making call

--- smile_detection.py
+++ smile_detection.py
@@ -3,4 +3,10 @@ import cv2
 # face classifier
 face_classifier = cv2.CascadeClassifier('haarscascade_frontalface_default.xml')
 
+# webcam feed
+webcam = cv2.VideoCapture(0)
+
+successful_frame, frame = webcam.read()
+
+
 print('successful')
